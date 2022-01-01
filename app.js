ba739b5f382f7ed433699ab3a82376f822ba48d3
EdAlegrid <30911092+EdAlegrid@users.noreply.github.com>
2022-01-01 16:23:33 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -3,16 +3,12 @@
 var path = require('path');
 var logger = require('morgan');
 var express = require('express');
-var client = require('./m2m/client.js');
-
 var app = express();
+var indexRoute = require('./routes/index.js');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.static('public'));
-
-var indexRoute = require('./routes/index.js');
-
 app.use('/', indexRoute);
 
 app.listen(4000);
