7ab880f75e728b5da5b108e2769f4de3dba59b52
EdAlegrid <30911092+EdAlegrid@users.noreply.github.com>
2022-01-02 14:52:41 -0500

    Update device.js

--- device1/device.js
+++ device1/device.js
@@ -6,9 +6,12 @@ let myData = 'myData';
 
 device.connect('https://www.node-m2m.com', () => {
 
+  //device.setGpio({mode:'input', pin:[11, 13]}, (gpio) => console.log(gpio.pin, gpio.state) );
+  device.setGpio({mode:'input', pin:[11, 13], type:'simulation'}, (gpio) => console.log(gpio.pin, gpio.state) );
+  
   //device.setGpio({mode:'output', pin:[33, 35]}); // raspberry pi
   device.setGpio({mode:'output', pin:[33, 35], type:'simulation'}); // windows or linux
-  
+    
   device.setData('get-data', (data) => {
     data.send(myData);
   });
